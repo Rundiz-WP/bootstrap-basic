2f6c2bbe3f4083c861f4afd3a08d5076be320a37
vee w <kawewong@gmail.com>
2015-10-23 11:28:22 +0700

    add donations link

--- inc/views/BootstrapBasicAdminHelp_v.php
+++ inc/views/BootstrapBasicAdminHelp_v.php
@@ -14,4 +14,6 @@
 	Cloak video element (video element or embeded video) with <code>&lt;div class=&quot;flexvideo&quot;&gt;...your video...&lt;/div&gt;</code>.
 
 	<?php do_action('bootstrapbasic_theme_help_content'); ?>
+	
+	<p style="margin-top: 20px;"><span style="font-size: 1.2rem;">&#128147;</span> If my theme can safe your life, your jobs, your projects please <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=9HQE4GVV4KTZE" target="donations_link">buy me some foods</a>.</p>
 </div>
