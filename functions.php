bc43ce3b5756595dc13ec57d9315d4c9b91f1621
vee w <kawewong@gmail.com>
2016-05-09 21:47:15 +0700

    conditional statement for HTML5Shiv & Respond.JS
    https://github.com/Rundiz/bootstrap-basic/issues/10
    
    add conditional statement for HTML5Shiv and Respond.JS
    Thanks to @coliff

--- functions.php
+++ functions.php
@@ -132,14 +132,20 @@ if (!function_exists('bootstrapBasicEnqueueScripts')) {
 	 */
 	function bootstrapBasicEnqueueScripts() 
 	{
+		global $wp_scripts;
+
 		wp_enqueue_style('bootstrap-style', get_template_directory_uri() . '/css/bootstrap.min.css', array(), '3.3.6');
 		wp_enqueue_style('bootstrap-theme-style', get_template_directory_uri() . '/css/bootstrap-theme.min.css', array(), '3.3.6');
 		wp_enqueue_style('fontawesome-style', get_template_directory_uri() . '/css/font-awesome.min.css', array(), '4.5.0');
 		wp_enqueue_style('main-style', get_template_directory_uri() . '/css/main.css');
 
 		wp_enqueue_script('modernizr-script', get_template_directory_uri() . '/js/vendor/modernizr.min.js', array(), '3.3.1');
-		wp_enqueue_script('respond-script', get_template_directory_uri() . '/js/vendor/respond.min.js', array(), '1.4.2');
-		wp_enqueue_script('html5-shiv-script', get_template_directory_uri() . '/js/vendor/html5shiv.min.js', array(), '3.7.3');
+		wp_register_script('respond-script', get_template_directory_uri() . '/js/vendor/respond.min.js', array(), '1.4.2');
+		$wp_scripts->add_data('respond-script', 'conditional', 'lt IE 9');
+		wp_enqueue_script('respond-script');
+		wp_register_script('html5-shiv-script', get_template_directory_uri() . '/js/vendor/html5shiv.min.js', array(), '3.7.3');
+		$wp_scripts->add_data('html5-shiv-script', 'conditional', 'lte IE 9');
+		wp_enqueue_script('html5-shiv-script');
 		wp_enqueue_script('jquery');
 		wp_enqueue_script('bootstrap-script', get_template_directory_uri() . '/js/vendor/bootstrap.min.js', array(), '3.3.6', true);
 		wp_enqueue_script('main-script', get_template_directory_uri() . '/js/main.js', array(), false, true);
