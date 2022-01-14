b24c4aa1943309585f811fc23ad554bf66e07e27
ve3 <kawewong@gmail.com>
2022-01-14 16:51:43 +0700

    Wrap calendar table with responsive table class.

--- inc/template-widgets-hook.php
+++ inc/template-widgets-hook.php
@@ -7,19 +7,20 @@
 
 
 if (!function_exists('bootstrapBasicWidgetHooksGetCalendar')) {
-	/**
-	 * change WordPress calendar widget table to add bootstrap class into it.
-	 * 
-	 * @todo change code in this function when WordPress allowed to hook into that widget.
-	 * @param string $calendar
-	 * @return string
-	 */
-	function bootstrapBasicWidgetHooksGetCalendar($calendar) 
-	{
-		$new_calendar = preg_replace('#(<table*\s)(id="wp-calendar")#i', '$1 id="wp-calendar" class="table"', $calendar);
+    /**
+     * change WordPress calendar widget table to add bootstrap class into it.
+     * 
+     * @todo change code in this function when WordPress allowed to hook into that widget.
+     * @param string $calendar
+     * @return string
+     */
+    function bootstrapBasicWidgetHooksGetCalendar($calendar) 
+    {
+        $new_calendar = preg_replace('#(<table*\s)(id="wp-calendar")#i', '$1 id="wp-calendar" class="table"', $calendar);
+        $new_calendar = '<div class="table-responsive">' . $new_calendar . '</div>';
 
-		return $new_calendar;
-	}// bootstrapBasicWidgetHooksGetCalendar
+        return $new_calendar;
+    }// bootstrapBasicWidgetHooksGetCalendar
 }
 add_filter('get_calendar', 'bootstrapBasicWidgetHooksGetCalendar', 10, 1);
 
