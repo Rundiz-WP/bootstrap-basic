4be3e3bfcfc5882b4c9f0a6576f5e6230a26569f
ve3 <kawewong@gmail.com>
2022-12-29 13:49:53 +0700

    Update theme help page to properly use translation.

--- inc/views/BootstrapBasicAdminHelp_v.php
+++ inc/views/BootstrapBasicAdminHelp_v.php
@@ -1,23 +1,38 @@
 <div class="wrap">
-    <h2>Bootstrap Basic help</h2>
+    <h2><?php _e('Bootstrap Basic help', 'bootstrap-basic'); ?></h2>
 
-    <h3>Menu</h3>
-    <p>To display menu correctly, please create at least 1 menu and set as primary and save.</p>
+    <h3><?php _e('Menu', 'bootstrap-basic'); ?></h3>
+    <p><?php _e('To display menu correctly, please create at least 1 menu and set as primary and save.', 'bootstrap-basic'); ?></p>
 
-    <h3>Bootstrap features</h3>
-    <p>This theme can use all Bootstrap classes, elements and styles.</p>
+    <h3><?php _e('Bootstrap features', 'bootstrap-basic'); ?></h3>
+    <p><?php _e('This theme can use all Bootstrap classes, elements and styles.', 'bootstrap-basic'); ?></p>
 
-    <h3>Responsive image</h3>
-    <p>For responsive image please add img-responsive class to img element. Example: <code>&lt;img src=&quot;...&quot; alt=&quot;&quot; class=&quot;img-responsive&quot;&gt;</code></p>
+    <h3><?php _e('Responsive image', 'bootstrap-basic'); ?></h3>
+    <p><?php 
+    printf(
+        /* translators: %1$s sample of responsive image HTML with class. */
+        __('For responsive image please add img-responsive class to img element. Example: %1$s', 'bootstrap-basic'),
+        '<code>&lt;img src=&quot;...&quot; alt=&quot;&quot; class=&quot;img-responsive&quot;&gt;</code>'
+    );
+    ?></p>
 
-    <h3>Responsive video</h3>
-    Cloak video element (video element or embeded video) with <code>&lt;div class=&quot;flexvideo&quot;&gt;...your video...&lt;/div&gt;</code>.
+    <h3><?php _e('Responsive video</h3>', 'bootstrap-basic'); ?>
+    <?php
+    printf(
+        /* translators: %1$s sample of responsive video HTML with class. */
+        __('Cloak video element (video element or embeded video) with %1$s.', 'bootstrap-basic'),
+        '<code>&lt;div class=&quot;flexvideo&quot;&gt;...&lt;/div&gt;</code>'
+    );
+    ?>
 
     <h3><?php _e('Bootstrap Basic Change log', 'bootstrap-basic'); ?></h3>
     <p>
             <?php 
-            /* translators: %1$s: Open link, %2$s: Close link */
-            echo sprintf(__('You can see what was changed in each version or each commits on our %1$sGithub page%2$s.', 'bootstrap-basic'), '<a href="https://github.com/Rundiz-WP/bootstrap-basic" target="bb_commits">', '</a>'); 
+            printf(
+                /* translators: %1$s: Open link, %2$s: Close link */
+                __('You can see what was changed in each version or each commits on our %1$sGithub page%2$s.', 'bootstrap-basic'), 
+                '<a href="https://github.com/Rundiz-WP/bootstrap-basic" target="bb_commits">', '</a>'
+            ); 
             ?> 
             <?php _e('You can also see it on changelog.md file that come with the theme.', 'bootstrap-basic'); ?> 
     </p>
